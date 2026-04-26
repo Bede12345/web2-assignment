92f7c231f5d46ac3687cdb32240c928f04a89044
Bede12345 <148060695+Bede12345@users.noreply.github.com>
2026-04-26 12:55:20 +0300

    Add comprehensive testing and documentation

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-s
+
     if (method === 'OPTIONS') {
         res.writeHead(200);
         res.end();
